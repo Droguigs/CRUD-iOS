bdcfefd7caab0df78867a77ae314a8656a773371
Douglas Schiavi <schiavi.douglas@gmail.com>
2020-07-09 13:02:08 -0300

    Search field fixes

--- CRUD-iOS/Controllers/ClientListViewController.swift
+++ CRUD-iOS/Controllers/ClientListViewController.swift
@@ -12,11 +12,17 @@ protocol NewClientDelegate {
     func addClient()
 }
 
+protocol ClientDelegate {
+    func editClient(row: Int)
+    func deleteClient(row: Int)
+}
+
 class ClientListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var noClientView: UIView!
-    var clientList: [Client] = []
+    
+    var filterredClients: [Client] = []
     let cache = ClientCache()
     
     override func viewDidLoad() {
@@ -33,9 +39,9 @@ class ClientListViewController: UIViewController {
     }
     
     func loadData() {
-        clientList = cache.allClients()
-        clientList = clientList.sorted(by: { $0.name < $1.name })
-        if clientList.isEmpty {
+        filterredClients = cache.allClients()
+        filterredClients = filterredClients.sorted(by: { $0.name < $1.name })
+        if filterredClients.isEmpty {
             self.noClientView.isHidden = false
         } else {
             self.tableView.reloadData()
@@ -66,18 +72,20 @@ class ClientListViewController: UIViewController {
 extension ClientListViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return clientList.count + 1
+        return filterredClients.count + 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         switch indexPath.row {
         case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell", for: indexPath) as! SearchViewControllerCell
+
+            cell.textSearchBar.delegate = self
             return cell
         default:
             let cell = tableView.dequeueReusableCell(withIdentifier: "ClientCell", for: indexPath) as! ClientViewControllerCell
             
-            cell.name.text = getUserTableName(client: clientList[indexPath.row-1])
+            cell.name.text = getUserTableName(client: filterredClients[indexPath.row-1])
 
             return cell
         }
@@ -85,7 +93,7 @@ extension ClientListViewController: UITableViewDelegate, UITableViewDataSource {
     
 }
 
-extension ClientListViewController: NewClientDelegate {
+extension ClientListViewController: NewClientDelegate, ClientDelegate {
     
     func addClient() {
         showToast(message: "Cliente cadastrado com sucesso", font: .systemFont(ofSize: 15), color: .systemGreen)
@@ -111,13 +119,53 @@ extension ClientListViewController: NewClientDelegate {
         })
     }
     
+    func editClient(row: Int) {
+        <#code#>
+    }
+    
+    func deleteClient(row: Int) {
+        <#code#>
+    }
+    
 }
 
-class SearchViewControllerCell: UITableViewCell { }
+extension ClientListViewController: UITextFieldDelegate {
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0)) as! SearchViewControllerCell as SearchViewControllerCell
+        cell.textSearchBar.resignFirstResponder()
+        self.filterredClients = cache.allClients()
+        if filterredClients.isEmpty {
+            self.noClientView.isHidden = false
+        } else {
+            self.tableView.reloadData()
+        }
+        return true
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.filterredClients = cache.allClients(cpf: textField.text ?? "")
+        if filterredClients.isEmpty {
+            self.noClientView.isHidden = false
+        } else {
+            self.tableView.reloadData()
+        }
+        return true
+    }
+}
+
+class SearchViewControllerCell: UITableViewCell {
+    @IBOutlet weak var textSearchBar: UITextField!
+}
 
 class ClientViewControllerCell: UITableViewCell {
     
     @IBOutlet weak var name: UILabel!
     
+    @IBAction func editClient(_ sender: Any) {
+        
+    }
     
+    @IBAction func deleteClient(_ sender: Any) {
+        
+    }
 }
